6a18769b3141aa2399a9d178397793ed5e9020b8
zubinraj <mangachalil@gmail.com>
2013-08-06 22:10:47 +0530

    Stripped out everything except for the core functionality of reconnecting and keep alive to run on raspberry pi.

--- auto-login.sh
+++ auto-login.sh
@@ -30,23 +30,17 @@ pathtotestfile="http://www.google.co.in"
 # the password as well 
 username=<username>
 password=<password>
+
 user_agent="Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5"
-program_folder=~/.auto-login
-# Enter time in 24 hr format.
-# Will safely handly case where start > stop eg: start 23:00 stop 06:00
-free_start="02:10"
-free_stop="07:50"
-# Default connection time out interval
-conn_timeout=300
+program_folder=~/logfiles
+# Default connection time out interval - connection timeout from asianet is 5 minutes, ping slightly before
 ping_interval=290
-ntp_wait=310
 
 # Initialize file paths
 
 lock_file=$program_folder/conn_url
 log_file=$program_folder/conn.log
 debug_log_file=$program_folder/debug.log
-script_folder=$program_folder
 
 # This program requires curl. Test for that
 which curl > /dev/null
@@ -70,19 +64,12 @@ fi
 if [ ! -d $program_folder ]; 
 then
   mkdir $program_folder
-  mkdir $program_folder/startup
-  mkdir $program_folder/shutdown
 fi
 if [ ! -d $program_folder ]; 
 then
   echo "Could not create program folder"
   exit
 fi
-
-# Initialize time vars
-now_utc=`date +%s`
-free_start_utc=`date +%s -d "$today $free_start"`
-free_stop_utc=`date +%s -d "$today $free_stop"`
 
 #
 #-BEGIN-FUNCTIONS-------------------------------------------------------
@@ -132,8 +119,8 @@ get_asianet_conn_url() {
     then
       cat $lock_file
     else
-      log_to_file "Using fallback URL: https://mwcp-tvm-04.adlkerala.com:8001/"   
-      echo https://mwcp-tvm-04.adlkerala.com:8001/
+      log_to_file "Using fallback URL: https://mwcp-ekm-04.adlkerala.com:8001/"   
+      echo https://mwcp-ekm-04.adlkerala.com:8001/
     fi
   fi
 }
@@ -173,281 +160,29 @@ keep_alive() {
 }
 
 #
-# Recalculate the global time variables
-#
-recalculate_time_vars() {
-  # We have to recalculate variables every time
-  today=`date +%x`
-  now_utc=`date +%s`
-  free_start_utc=`date +%s -d "$today $free_start"`
-  free_stop_utc=`date +%s -d "$today $free_stop"`
-  # For safety we calculate free download start time+timeout
-  free_start_utc=$(($free_start_utc+$conn_timeout))
-  # For safety we calculate free download stop time-timeout
-  free_stop_utc=$(($free_stop_utc-$conn_timeout))
-  # If free time stop is less than free time start then these are in two dates
-  # Eg: start = 23:00 stop = 06:00 taking the times for the current day we get
-  # start > stop. So we add 86400 to take stop to the next day
-  if [ $free_stop_utc -lt $free_start_utc ];
-  then
-    # If now < free_stop then flip the start time back
-    if [ $now_utc -le $free_stop_utc ];
-    then
-      free_start_utc=$(($free_stop_utc-86400)) 
-    # If now > free_start then flip the stop time forward
-    elif [ $now_utc -ge $free_start_utc ]; 
-    then 
-      free_stop_utc=$(($free_stop_utc+86400)) 
-    fi
-  else  
-    # If free download time is over for the day look forward to the next free slot  
-    if [ $now_utc -gt $free_stop_utc ];
-    then
-      free_start_utc=$(($free_start_utc+86400)) 
-      free_stop_utc=$(($free_stop_utc+86400)) 
-    fi
-  fi
-}
-
-#
-# Get the number of seconds to sleep before free connection starts
-#
-time_to_free_start() {
-  recalculate_time_vars
-  # If it is between the free download slot just wait the default timeout
-  if [ $now_utc -lt $free_stop_utc -a $now_utc -ge $free_start_utc ];
-  then
-    echo $conn_timeout
-  else
-    # If free download time is yet to start for the day
-    if [ $now_utc -lt $free_start_utc ];
-    then
-      echo $(($free_start_utc-$now_utc))  
-    fi 
-  fi  
-}
-
-#
-# Check if we are within the free download time.
-#
-is_still_free_download_time() {
-  recalculate_time_vars
-  # If it is between the free download slot just wait the default timeout
-  if [ $now_utc -lt $free_stop_utc -a $now_utc -gt $free_start_utc ];
-  then
-    return 0
-  else
-    return 1
-  fi  
-}
-
-#
 #-END-FUNCTIONS---------------------------------------------------------
 
-if [ "$1" == "free" ];
-then 
-  if ! is_connected;
-  then
-    # If not connected then connect
-    connect
-    # Wait for an ntpdate call from cron
-    sleep $ntp_wait
-    disconnect
-    sleep 1
-  else
-    # Wait for an ntpdate call from cron
-    sleep $ntp_wait
-    sleep 1
-  fi  
-fi
-case "$1" in
-  "auto"|"startup"|"free")
-    # If this is a startup run wait 5 minutes to make sure that the last connection
-    # times out. To take care of a quick power outage and quick return of power
-    # before the previous connection times out
-    if [ "$1" == "startup" -o "$1" == "free" ];
-    then 
-      log "Starting application."
-      if [ "$1" == "free" ];
-      then 
-        # If this is a free download run, sleep till the free time starts
-        sleep_time=$(time_to_free_start)
-        if is_still_free_download_time;
-        then
-          # If within free download time say so
-          log "Within free time, sleeping for $sleep_time second(s)."
-        else  
-          log "Sleeping for $sleep_time second(s) till free time starts."
-        fi
-        sleep $sleep_time
-      else
-        if [ "$2" != "nowait" ];
-        then 
-          log "Sleeping for $conn_timeout second(s) for old connection (if any) to time out."
-          sleep $conn_timeout
-        fi  
-      fi    
-    fi 
-    # Check if connected and connect if not
-    if ! is_connected;
-    then
-      # If not connected then connect
-      connect
-      if ! is_connected;
-      then
-        # If connection attempt failed log and exit 
-        log "Could not connect to the connection."
-        exit
-      else
-        log "Successfully connected to the connection."
-      fi  
-    else
-      # In auto mode disconnect if connected
-      if [ "$1" == "auto" ];
-      then
-        # If connected to the internet disconnect and exit
-        disconnect
-        if is_connected;
-        then
-          log "Could not disconnect the connection."
-        else
-          log "Successfully disconnected the connection."
-        fi
-        exit
-      fi  
-    fi
-    # If this is a free run then run any additional scripts
-    if [ "$1" == "free" ];
-    then 
-      if [ -d $script_folder ]; 
-      then
-        log "Running startup scripts."
-        for script in $script_folder/startup/*; 
-        do
-          log "Running $script."
-          if [ -x $script ]; then $script; fi
-        done
-      fi  
-    fi 
-    # If conrol reaches here, connection exists, keep alive
-    while [ 1 ];
-    do
-      # If not explicitly logged out keep connection live
-      # the autologin file would have been deleted in a logout operation
-      if [ -f $lock_file ];
-      then
-        if is_connected;
-        then
-          # If connected then proceed
-          keep_alive
-          if ! is_connected;
-          then
-            log "Something is wrong, you are not connected to the internet."
-            exit
-          fi  
-          # When lock file exists but there is no connection try reconnect  
-        else
-          connect
-          if ! is_connected;
-          then
-            log "Could not re-connect."
-          else
-            log "Successfully re-connected."
-          fi  
-        fi
-      else  
-        # Lock file has been explicitly deleted during a disconnect operation
-        # So exit  
-        log "Disconnected; probably from another thread, exiting."
-        exit
-      fi
-      # If this is a free run then exit at end of free time
-      if [ "$1" == "free" ];
-      then      
-        if ! is_still_free_download_time;
-        then
-          log "Free Download time over, disconnecting"
-          if [ -d $script_folder ]; 
-          then
-            log "Running shutdown scripts."
-            for script in $script_folder/shutdown/*; 
-            do
-              log "Running $script."
-              if [ -x $script ]; then $script; fi
-            done
-          fi  
-          disconnect
-          if is_connected;
-          then
-            log "Could not disconnect the connection."
-          else
-            log "Successfully disconnected the connection."
-          fi    
-          # In any case exit
-          exit      
-        fi  
-      fi
-      # Sleep ping_interval and ping again
-      sleep $ping_interval
-    done
-    ;;
-  "keep-alive")
-    # Check if connected
-    if is_connected;
-    then
-      # If connected then proceed
-      keep_alive
-      if ! is_connected;
-      then
-        log "Something is wrong, you are not connected to the connection"
-      fi  
-    else
-      log "This system is disconnected from the internet. Cannot send keep-alive request."
-    fi
-    ;;
-  "logout")
-    # Check if connected and proceed if connected
-    if is_connected;
-    then
-      disconnect
-      if is_connected;
-      then
-        log "Could not disconnect the connection."
-      else
-        log "Successfully disconnected the connection."
-      fi  
-    else
-      log "This system is already disconnected from the internet."
-    fi
-    ;;
-  "--help")
-    echo "----------------------------------------------------------------"
-    echo "Asianet Auto Login Script"
-    echo "Copyright (c) 2009-12 Anoop John, Prasad S. R. (www.zyxware.com)"
-    echo "----------------------------------------------------------------"
-    echo "Usage: "
-    echo `basename $0` "[auto|startup|free|logout|*][nowait]"
-    echo "  auto    - Logs in if not logged in, else logs out."
-    echo "  startup - Startup mode, keeps a connection always on."
-    echo "  free    - Keeps connection alive only during free download hours."
-    echo "  logout  - Disconnect."
-    echo "  *       - Connect."
-    echo "  nowait  - Do not wait for any connection to expire."
-    ;;
-  *)
-    # Check if already connected and proceed if not connected
-    if ! is_connected;
-    then
-      connect
-      if ! is_connected;
-      then
-        log "Could not connect to the connection."
-      else
-        log "Successfully connected to the connection."
-      fi  
-    else
-      log "This system is already connected to the internet."
-    fi
-    ;;
+log "System started."
+
+# If disconnect, try connecting. If connected, keep alive.
+while [ 1 ];
+do
+	if is_connected;
+	then
+	  # If connected then proceed
+	  keep_alive
+	else
+	  connect
+	  if ! is_connected;
+	  then
+		log "Could not connect."
+	  else
+		log "Successfully re-connected."
+	  fi  
+	fi
+  # Sleep ping_interval and ping again
+  sleep $ping_interval
+done
+;;
 esac
 
